aeba608c02a4e403bddd48afa2a3835db2fc85e5
muhamedalqemam <muhamed.alqemam@gmail.com>
2024-03-30 17:32:20 +0200

    v1.02

--- HelloJava/src/secondPackage/SPfirstClass.java
+++ HelloJava/src/secondPackage/SPfirstClass.java
@@ -3,7 +3,8 @@ package secondPackage;
 public class SPfirstClass {
 
 	public static void main(String[] args) {
-		System.out.println("Second Package First Class");
+		System.out.println("Second Package First Class "
+				+ "Test");
 	}
 
 }
